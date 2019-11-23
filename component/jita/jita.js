363f89f4fe455948c1a97d23e64a21cccba385cc
zhuangjiawei <192992377@qq.com>
2019-11-23 22:29:19 +0800

    初始化项目

--- component/jita/jita.js
+++ component/jita/jita.js
@@ -3,23 +3,48 @@ Component({
   /**
    * 组件的属性列表
    */
-  properties: {
-
-  },
+  // properties: {
+  //   propA: {
+  //     type: string
+  //   }
+  // },
 
   /**
    * 组件的初始数据
    */
   data: {
-    list: [
-      { src: "./leqi.png", text: "民谣吉他" },
-      { src: "./leqi.png", text: "指弹吉他" },
-      { src: "./leqi.png", text: "电吉他" },
-      { src: "./leqi.png", text: "琴包" },
-      { src: "./leqi.png", text: "最近练习" },
-      { src: "./leqi.png", text: "微店" },
-      { src: "./leqi.png", text: "教学" },
-      { src: "./leqi.png", text: "贝斯" }
+    list: [{
+        src: "./leqi.png",
+        text: "民谣吉他"
+      },
+      {
+        src: "./leqi.png",
+        text: "指弹吉他"
+      },
+      {
+        src: "./leqi.png",
+        text: "电吉他"
+      },
+      {
+        src: "./leqi.png",
+        text: "琴包"
+      },
+      {
+        src: "./leqi.png",
+        text: "最近练习"
+      },
+      {
+        src: "./leqi.png",
+        text: "微店"
+      },
+      {
+        src: "./leqi.png",
+        text: "教学"
+      },
+      {
+        src: "./leqi.png",
+        text: "贝斯"
+      }
     ]
   },
 
