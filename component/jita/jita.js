833c0d49822fe22581a942d6c828bf703a35fded
zhuangjiawei <192992377@qq.com>
2019-11-25 22:32:38 +0800

    index首页通过prop传值

--- component/jita/jita.js
+++ component/jita/jita.js
@@ -3,55 +3,68 @@ Component({
   /**
    * 组件的属性列表
    */
-  // properties: {
-  //   propA: {
-  //     type: string
-  //   }
-  // },
+  properties: {
+    jitalist: {
+      type: Array,
+      value: [],
+      observer: function(newVal, oldVal, changePath) {
+        // 属性被改变时执行的函数（可选），也可以写成在methods段中定义的方法名字符串
+        // 通常 newVal 就是新设置的数据， oldVal 是旧数据
+      }
+    }
+  },
 
   /**
    * 组件的初始数据
    */
   data: {
-    list: [{
-        src: "./leqi.png",
-        text: "民谣吉他"
-      },
-      {
-        src: "./leqi.png",
-        text: "指弹吉他"
-      },
-      {
-        src: "./leqi.png",
-        text: "电吉他"
-      },
-      {
-        src: "./leqi.png",
-        text: "琴包"
-      },
-      {
-        src: "./leqi.png",
-        text: "最近练习"
-      },
-      {
-        src: "./leqi.png",
-        text: "微店"
-      },
-      {
-        src: "./leqi.png",
-        text: "教学"
-      },
-      {
-        src: "./leqi.png",
-        text: "贝斯"
-      }
-    ]
+    // list: [{
+    //     src: "./leqi.png",
+    //     text: "民谣吉他"
+    //   },
+    //   {
+    //     src: "./leqi.png",
+    //     text: "指弹吉他"
+    //   },
+    //   {
+    //     src: "./leqi.png",
+    //     text: "电吉他"
+    //   },
+    //   {
+    //     src: "./leqi.png",
+    //     text: "琴包"
+    //   },
+    //   {
+    //     src: "./leqi.png",
+    //     text: "最近练习"
+    //   },
+    //   {
+    //     src: "./leqi.png",
+    //     text: "微店"
+    //   },
+    //   {
+    //     src: "./leqi.png",
+    //     text: "教学"
+    //   },
+    //   {
+    //     src: "./leqi.png",
+    //     text: "贝斯"
+    //   }
+    // ]
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onTapChild: function(event) {
+      // detail对象，提供给事件监听函数
+      var myEventDetail = {
+        text: event.currentTarget.dataset.text
+      }
+      // 触发事件的选项
+      var myEventOption = {}
+      this.triggerEvent('parentEvent', myEventDetail, myEventOption)
+    }
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,46 @@ const app = getApp()
 
 Page({
   data: {
-     
+    jitaconent: [{
+        src: "./leqi.png",
+        text: "民谣吉他"
+      },
+      {
+        src: "./leqi.png",
+        text: "指弹吉他"
+      },
+      {
+        src: "./leqi.png",
+        text: "电吉他"
+      },
+      {
+        src: "./leqi.png",
+        text: "琴包"
+      },
+      {
+        src: "./leqi.png",
+        text: "最近练习"
+      },
+      {
+        src: "./leqi.png",
+        text: "微店"
+      },
+      {
+        src: "./leqi.png",
+        text: "教学"
+      },
+      {
+        src: "./leqi.png",
+        text: "贝斯"
+      }
+    ]
+  },
+  // 当自定义组件触发 parentEvent 事件时，调用 onParentEvent 方法
+  onParentEvent: function(event) {
+    // 自定义组件触发事件时提供的detail对象，用来获取子组件传递来的数据
+    var text = event.detail.text;
+    // console.log("event.detail", event.detail)
+    console.log('子组件传递来的数据 text:', text);
+    // 其他操作...
   }
 })
